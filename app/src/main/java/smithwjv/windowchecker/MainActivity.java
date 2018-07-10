32c9f8dbc173c71079f61415915a4f01ed7ef08e
Will JV Smith <smithwjv@gmail.com>
2018-07-10 18:45:07 +0100

    Add network communication with the Pi using HTTP
    A request is made to the Pi and a string is returned, if the Pi is running the server.

--- app/src/main/java/smithwjv/windowchecker/MainActivity.java
+++ app/src/main/java/smithwjv/windowchecker/MainActivity.java
@@ -1,13 +1,47 @@
 package smithwjv.windowchecker;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+
+import smithwjv.windowchecker.utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView piResponseTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        piResponseTextView = (TextView) findViewById(R.id.tv_pi_response);
+        new PiQueryTask().execute(NetworkUtils.getUrl(this));
+    }
+
+    public class PiQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL piUrl = urls[0];
+            String piResponse = null;
+            try {
+                piResponse = NetworkUtils.getResponseFromHttpUrl(piUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return piResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s!= null && !s.equals("")) {
+                piResponseTextView.setText(s);
+            }
+        }
     }
 }
---
+++ app/src/main/java/smithwjv/windowchecker/utilities/NetworkUtils.java
@@ -0,0 +1,56 @@
+package smithwjv.windowchecker.utilities;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+import smithwjv.windowchecker.R;
+
+public final class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    public static URL getUrl(Context mContext) {
+        URL url = null;
+        try {
+            url = new URL(mContext.getResources().getString(R.string.pi_address));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.v(TAG, "Built URL " + url);
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
